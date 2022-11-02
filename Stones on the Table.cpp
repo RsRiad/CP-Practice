53ab7e54b14e5705b4ac1c8e50e86ef8eacb8657
MD. RAWHA SIDDIQI RIAD <rsriad00@gmail.com>
2022-11-03 00:10:03 +0600

    Create Stones on the Table.cpp

---
+++ Stones on the Table.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int n,count=0;
+    cin>>n;
+    cin>>s;
+    for(int i=0; i<n; i++)
+        for(int j=i+1; j<n; j++)
+            if(s[i]==s[j] && (j-i)==1)
+                count++;
+
+    cout<<count;
+
+}
+
