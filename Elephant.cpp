bef72b1c614e047c61fa042e78a35c3217fd62ce
MD. RAWHA SIDDIQI RIAD <rsriad00@gmail.com>
2022-11-03 12:23:56 +0600

    Update Elephant.cpp

--- Elephant.cpp
+++ Elephant.cpp
@@ -5,9 +5,7 @@ int main()
     int x,rem,ans;
     cin>>x;
     rem=x/5;
-    if(x%5<5 && x%5!=0)
-        cout<<rem+1;
-
+    if(x%5<5 && x%5!=0) cout<<rem+1;
     else cout<<rem;
     return 0;
 }
