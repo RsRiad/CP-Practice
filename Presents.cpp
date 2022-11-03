c47994f4153912d080c5a3a34b5b17865a1a3cfd
MD. RAWHA SIDDIQI RIAD <rsriad00@gmail.com>
2022-11-04 01:15:17 +0600

    Create Presents.cpp

---
+++ Presents.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t,x,arr[100];
+int main()
+{
+    cin>>t;
+    for (int i=1; i<=t; i++)
+    {
+        cin >> x;
+        arr[x] = i;
+    }
+    for (int i=1; i<=t; i++) cout<<arr[i]<< " ";
+    return 0;
+}
