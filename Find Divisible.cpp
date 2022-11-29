3c83ead6dbfb6d60dceb2f548c430bdb6ff503ed
MD. RAWHA SIDDIQI RIAD <rsriad00@gmail.com>
2022-11-29 15:06:41 +0600

    Create Find Divisible.cpp

---
+++ Find Divisible.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int t;
+    cin>>t;
+    ll l,r;
+    while(t--)
+    {
+        cin>>l>>r;
+        cout<<l<<" "<<l*2<<"\n";
+    }
+
+    return 0;
+}
+
